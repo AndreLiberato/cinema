fc004022b38218717873599e7088926745e71357
André Liberato <andre.liberato@outlook.com.br>
2023-07-09 20:59:51 -0300

    Adicionado navegação para Cinemas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('cinema.index')" :active="request()->routeIs('cinema.*')">
+                        {{ __('Cinemas') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -69,6 +73,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('cinema.index')" :active="request()->routeIs('cinema.*')">
+                {{ __('Cinemas') }}
             </x-responsive-nav-link>
         </div>
 
