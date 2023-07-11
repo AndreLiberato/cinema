556273cc640a9354e2bb82e2ff199b5e7201aa2e
André Liberato <andre.liberato@outlook.com.br>
2023-07-11 18:55:28 -0300

    Validação de requisição da sala

---
+++ app/Http/Requests/SalaUpdateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SalaUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'numero' => ['integer', 'required'],
+            'tipo' => ['string', 'required', 'max:32'],
+            'capacidade' => ['integer', 'required'],
+            'cinema_id' => ['integer', 'required', 'exists:cinema,id']
+        ];
+    }
+}
