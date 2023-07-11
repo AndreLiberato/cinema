cfa33ec1461d36d374ecebf6fc1da6f65589e33e
André Liberato <andre.liberato@outlook.com.br>
2023-07-11 18:57:19 -0300

    Pequenos ajustes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,6 +19,10 @@
                     <x-nav-link :href="route('cinema.index')" :active="request()->routeIs('cinema.*')">
                         {{ __('Cinemas') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('sala.index')" :active="request()->routeIs('sala.*')">
+                        {{ __('Salas') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -74,8 +78,13 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
             <x-responsive-nav-link :href="route('cinema.index')" :active="request()->routeIs('cinema.*')">
                 {{ __('Cinemas') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('sala.index')" :active="request()->routeIs('sala.*')">
+                {{ __('Salas') }}
             </x-responsive-nav-link>
         </div>
 
