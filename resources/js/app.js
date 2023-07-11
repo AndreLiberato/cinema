99b89679f0849236f45f95493d6c770581870662
André Liberato <andre.liberato@outlook.com.br>
2023-07-11 19:02:01 -0300

    Adicionado o uso de algumas ferramentas necessárias no front

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import { Tooltip, Select, Datepicker, Input, initTE } from "tw-elements";
 
+initTE({ Tooltip, Select, Datepicker, Input });
+
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-te-toggle="tooltip"]'));
+tooltipTriggerList.map((tooltipTriggerEl) => new Tooltip(tooltipTriggerEl));
 window.Alpine = Alpine;
 
 Alpine.start();
